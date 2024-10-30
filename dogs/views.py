f076ba97e7433c8d625f5d416c1b7866ed4b1fc0
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-10-30 11:41:48 +0300

    Добавлена возможность редактирование питомца только владельцу или администратору
    Добавлены ограничения доступа к функциям только для зарегестрированных пользователей

--- dogs/views.py
+++ dogs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, CreateView,  DetailView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin #миксин который выполняет классы только тогда когда пользователь зарегестрированный
+from django.http import Http404
 
 from dogs.models import Category, Dog
 from dogs.forms import DogForm
@@ -16,6 +19,7 @@ def index(request):
     return render(request, 'dogs/index.html', context)
 
 
+@login_required
 def categories(request):
     """ Показывает страницу с информацией о всех категориях питомника."""
     context = {
@@ -24,7 +28,7 @@ def categories(request):
     }
     return render(request, 'dogs/categories.html', context)
 
-
+@login_required
 def category_dogs(request, pk):
     """ Показывает страницу с информацией о питомцах определенной категории."""
     category_item = Category.objects.get(pk=pk)
@@ -35,29 +39,39 @@ def category_dogs(request, pk):
     }
     return render(request, 'dogs/dogs.html', context)
 
+
 class DogListView(ListView):
     """ Показывает список всех питомцев."""
     model = Dog
-    extra_context ={
+    extra_context = {
         'title': "Питомник - Все наши собаки",
     }
     template_name = 'dogs/dogs.html'
 
 
-class DogCreateView(CreateView):
+class DogCreateView(LoginRequiredMixin, CreateView):
     """ Страница создания нового питомца."""
     model = Dog
     form_class = DogForm
     template_name = 'dogs/create.html'
     success_url = reverse_lazy('dogs:list_dogs')
 
-class DogDetailView(DetailView):
+    def form_valid(self, form):
+        # Добавляем текущего пользователя в поле "владелец" нового питомца
+        # form.instance.owner = self.request.user
+        self.object = form.save # получаем объект из формы
+        self.object.owner = self.request.user # добавляем владельца собаки из зарегистрированого пользователя
+        self.object.save()
+        return super().form_valid(form)
+
+
+class DogDetailView(LoginRequiredMixin, DetailView):
     """ Страница детальной информации о питомца."""
     model = Dog
     template_name = 'dogs/detail.html'
 
 
-class DogUpdateView(UpdateView):
+class DogUpdateView(LoginRequiredMixin, UpdateView):
     """ Страница изменения информации о питомце."""
     model = Dog
     form_class = DogForm
@@ -65,11 +79,19 @@ class DogUpdateView(UpdateView):
 
     def get_success_url(self):
         # return reverse('dogs:detail_dog', args=[self.object.pk])  # Переходим на страницу детальной информации питомца после редактирования
-        return reverse('dogs:detail_dog', args=[self.kwargs.get('pk')])  # Переходим на страницу детальной информации питомца после редактирования
-
+        # Переходим на страницу детальной информации питомца после редактирования
+        return reverse('dogs:detail_dog', args=[self.kwargs.get('pk')])
+    
+    def get_object(self, queryset=None):
+        # Если питомец не принадлежит текущему пользователю или не пользователю администратор, то возбуждаем ошибку
+        self.object = super().get_object(queryset)
+        if self.object.owner != self.request.user and not self.request.user.is_staff:
+            raise Http404
+        return self.object
 
 class DogDeleteView(DeleteView):
     """ Страница удаления питомца."""
     model = Dog
     template_name = 'dogs/delete.html'
-    success_url = reverse_lazy('dogs:list_dogs')  # Переходим на страницу со списком питомцев после удаления
+    # Переходим на страницу со списком питомцев после удаления
+    success_url = reverse_lazy('dogs:list_dogs')
