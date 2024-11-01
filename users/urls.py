29cd849ca1ee9b23825fde922f236941e5b61275
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-01 10:59:10 +0300

    Исправлена ошибка as_view()

--- users/urls.py
+++ users/urls.py
@@ -7,11 +7,11 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('', UserLoginView.as_view(), name="login_user"),
-    path('logout/', UserLogoutView.as_view, name='logout_user'),
+    path('logout/', UserLogoutView.as_view(), name='logout_user'),
     path('register/', UserRegisterView.as_view(), name='register_user'),
-    path('profile/', UserProfileView.as_view, name='profile_user'),
-    path('update/', UserUpdateView.as_view, name='update_user'),
-    path('change_password/', UserPasswordChangeView.as_view, name='change_password_user'),
+    path('profile/', UserProfileView.as_view(), name='profile_user'),
+    path('update/', UserUpdateView.as_view(), name='update_user'),
+    path('change_password/', UserPasswordChangeView.as_view(), name='change_password_user'),
     path('profile/genpassword/', user_generate_new_password, name='generate_new_password_user')
 
 ]
