fba1ccb9783bcdb0efba44bc61ce429a081627de
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-01 16:15:50 +0300

    исправлены настройки redis

--- config/settings.py
+++ config/settings.py
@@ -203,7 +203,7 @@ CACHE_ENABLED = os.getenv('CACHE_ENABLED') == 'True'
 if CACHE_ENABLED:
     CACHES = {
         'default': {
-            'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
             'LOCATION': os.getenv('CACHE_LOCATION'),
         }
     }
