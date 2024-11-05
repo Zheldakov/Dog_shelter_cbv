163f93f39b2d4163c48abc6bba5d236da7b4e4af
Andrey Zheldakov <zheldakov.aa@gmail.com>
2024-11-05 11:53:49 +0300

    Добавлено отображение в админки,  права пользователя is_staff

--- users/admin.py
+++ users/admin.py
@@ -3,5 +3,5 @@ from users.models import User
 # Регистрируем модель User в административной панели Django
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email',  'is_active', 'role', 'pk', 'is_active') # Показываем поля в списке модели
+    list_display = ('first_name', 'last_name', 'email',  'is_active', 'role', 'pk', 'is_active', 'is_staff') # Показываем поля в списке модели
     list_filter = ('last_name',) # Фильтрация по полю last_name
