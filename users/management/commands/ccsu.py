64305b12d671e9e372f70831051fbb367138b526
Andrey Zheldakov <zheldakov.aa@gmail.com>
2024-11-05 11:34:08 +0300

    Расширены возможности для разных уровней доступа пользователей (Админ, модератор, пользователь)

--- users/management/commands/ccsu.py
+++ users/management/commands/ccsu.py
@@ -8,14 +8,41 @@ from config.settings import SU_DJANGO_PASSWORD
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(
+        admin = User.objects.create(
             email='admin@web.top',
             first_name='Admin',
             last_name='Adminov',
+            role = 'admin',
             is_staff=True,
             is_superuser=True,
             is_active=True
         )
-        user.set_password(SU_DJANGO_PASSWORD)
-        user.save()
-        return 'Admin created'
+        admin.set_password(SU_DJANGO_PASSWORD)
+        admin.save()
+    print('Admin created')
+    
+    moderator = User.objects.create(
+        email='moderator@web.top',
+        first_name='Moderator',
+        last_name='Moderator',
+        role='moderator',
+        is_staff=True,
+        is_superuser=False,
+        is_active=True
+    )
+    moderator.set_password(SU_DJANGO_PASSWORD)
+    moderator.save()
+    print('Moderator created')
+    
+    user = User.objects.create(
+        email='user@web.top',
+        first_name='User',
+        last_name='User',
+        role='user',
+        is_staff=True,
+        is_superuser=False,
+        is_active=True
+    )
+    user.set_password(SU_DJANGO_PASSWORD)
+    user.save()
+    print('User created')
--- users/models.py
+++ users/models.py
@@ -1,14 +1,21 @@
 # Описание моделей
 from django.db import models
 from django.contrib.auth.models import  AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # настройка полей, чтобы возможно было заполнить поля пустыми
 NULLABLE ={'blank': True, 'null': True}
+
+class UserRoles(models.TextChoices):
+    ADMIN = 'admin', _('admin')
+    MODERATOR = 'moderator', _('moderator')
+    USER = 'user', _('user')
 
 class User(AbstractUser):
     """Модель пользователя с полями email, телефон и Telegram username."""
     username = None
     email = models.EmailField(unique=True,verbose_name='email')
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
     first_name = models.CharField(max_length=150, verbose_name='First Name',default="Anonymous")
     last_name = models.CharField(max_length=150, verbose_name='Last Name', default="Anonymous")
     phone = models.CharField(max_length=35, verbose_name='Phone number', **NULLABLE)
