b3ac2e488f86fd31ffe6f66ef9be4955e7cd059b
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-13 12:09:08 +0300

    Добавлена отправка на почту информация по просмотрам

--- dogs/services.py
+++ dogs/services.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.core.cache import cache
+from django.core.mail import send_mail
 
-from dogs.models import Category
+from dogs.models import Category, Dog
+
 
 def get_categories_cache():
     # Кеширование
@@ -12,7 +14,16 @@ def get_categories_cache():
             category_list = Category.objects.all()
             cache.set(key, category_list)
     else:
-            category_list = Category.objects.all()
+        category_list = Category.objects.all()
 
     return category_list
-        
+
+
+def send_views_mail(dog_object, owner_email, views_count ):
+    # Отправка письма с информацией о просмотрах
+    send_mail(
+        subject=f"{views_count} просмотров {dog_object}",
+        message=f"Ура! Уже {views_count}, просмотров записи {dog_object}",
+        from_email=settings.EMAIL_HOST_USER,  # Email address
+        recipient_list=[owner_email],  # Список получателей
+    )
