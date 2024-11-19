23da6b580a3f4bb88396f24f51f9c431c743be74
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-19 15:46:29 +0300

    Добавлены/исправлены шаблоны и url

--- reviews/urls.py
+++ reviews/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('review/detail/<slug:slug>/', ReviewDetailView.as_view(), name='detail_review'),
     path('review/update/<slug:slug>/', ReviewUpdateView.as_view(), name='create_update_review'),
     path('review/delete/<slug:slug>/', ReviewDeleteView.as_view(), name='delete_review'),
-    path('review/toggle/<slug:slug>/', review_toggle_activity, name='review_toggle_activity'),
+    path('review/toggle/<slug:slug>/', review_toggle_activity, name='toggle_activity_review'),
 ]
