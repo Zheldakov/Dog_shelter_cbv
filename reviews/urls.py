ccf9e7fa64f3d74d27b73fd4ee5825bdadebe9ab
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-19 12:12:13 +0300

    Добавлено отображение отзывов

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from reviews.apps import ReviewsConfig
-from reviews.views import CategoryReviewListView, DeactivatedDogReviewListView
+from reviews.views import ReviewListView, DeactivatedDogReviewListView
 
 app_name = ReviewsConfig.name  # for backwards compatibility
 
 urlpatterns = [
 
-    path('<int:pk>/reviews/',CategoryReviewListView.as_view(), name='reviews_list'),
-    path('<int:pk>/reviews/deactivated/',DeactivatedDogReviewListView.as_view(), name='deactivated_reviews_list'),
+    path('',ReviewListView.as_view(), name='list_reviews'),
+    # path('<int:pk>/reviews/',ReviewListView.as_view(), name='list_reviews'),
+    path('deactivated/',DeactivatedDogReviewListView.as_view(), name='deactivated_reviews_list'),
+    # path('<int:pk>/reviews/deactivated/',DeactivatedDogReviewListView.as_view(), name='deactivated_reviews_list'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,18 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
-
+from django.http import HttpResponseForbidden
+from django.shortcuts import  reverse, get_object_or_404,redirect
 from django.views.generic import CreateView, CreateView, DeleteView, UpdateView, ListView
+from django.core.exceptions import PermissionDenied
 
 from reviews.models import Review
+from users.models import UserRoles
+from reviews.forms import ReviewForm
 
 
-class CategoryReviewListView(LoginRequiredMixin, ListView):
+
+
+class ReviewListView(LoginRequiredMixin, ListView):
     model = Review
     extra_context = {
         'title': 'Питомник - Все отзывы по собаке'
@@ -15,7 +21,7 @@ class CategoryReviewListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         queryset = super().queryset()
-        queryset = queryset.filter(dog_pk=self.kwargs.get('pk'))
+        # queryset = queryset.filter(dog_pk=self.kwargs.get('pk'))
         queryset = queryset.filter(sign_of_review=True)
 
         return queryset
