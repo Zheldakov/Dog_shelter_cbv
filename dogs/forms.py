c8173711b352d10700c88b464031715f5d30c909
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-20 16:04:38 +0300

    Исправлены ошибки

--- dogs/forms.py
+++ dogs/forms.py
@@ -28,9 +28,18 @@ class DogAdminForm(StyleFromMixin, forms.ModelForm):
         model = Dog
         exclude = '__all__'
 
-    @staticmethod
-    def clean_birth_date():
-        DogForm.clean_birth_date()
+    # @staticmethod
+    # def clean_birth_date():
+    #     DogForm.clean_birth_date()
+    def clean_birth_date(self):
+        cleaned_data = self.cleaned_data['birth_date']
+        if cleaned_data is None:
+            return None
+        now_year = datetime.datetime.now().year
+        if now_year - cleaned_data.year > 100:
+            raise forms.ValidationError(
+                'Возраст питомца не может быть больше 100 лет')
+        return cleaned_data
 
 
 class ParentForm(StyleFromMixin, forms.ModelForm):
