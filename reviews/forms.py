b2343fda362511d011bbebe821ee8e331bf31722
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-20 16:04:30 +0300

    Обнавлена форма. Скрыто поле ссылки

--- reviews/forms.py
+++ reviews/forms.py
@@ -4,6 +4,10 @@ from dogs.forms import StyleFromMixin
 
 
 class ReviewForm(StyleFromMixin, forms.ModelForm):
+    # Создаем форму для добавления/редактирования отзыва с указанными полями
+    title=forms.CharField(max_length=150,label='Заголовок формы')
+    content=forms.TextInput()
+    slug=forms.SlugField(max_length=20,initial='temp_slug', widget=forms.HiddenInput())
     class Meta:
         model = Review
         fields = ('dog','title','content','slug')
