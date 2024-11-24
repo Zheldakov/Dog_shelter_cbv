c7732de055104cbfb1c16d383cbab41287da955b
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-24 21:16:02 +0300

    Исправление ошибок

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,9 +5,9 @@ from dogs.forms import StyleFromMixin
 
 class ReviewForm(StyleFromMixin, forms.ModelForm):
     # Создаем форму для добавления/редактирования отзыва с указанными полями
-    title=forms.CharField(max_length=150,label='Заголовок формы')
+    title=forms.CharField(max_length=150,label='Заголовок_формы')
     content=forms.TextInput()
-    slug=forms.SlugField(max_length=20,initial='temp_slug', widget=forms.HiddenInput())
+    slug=forms.SlugField(max_length=20, initial='temp_slug', widget=forms.HiddenInput())
     class Meta:
         model = Review
         fields = ('dog','title','content','slug')
--- reviews/utils.py
+++ reviews/utils.py
@@ -3,4 +3,4 @@ import random
 
 
 def slug_generation():
-    return ''.join(random.choice(string.ascii_lowercase + string.digits + string.ascii_uppercase, k=20))
+    return ''.join(random.choices(string.ascii_lowercase + string.digits + string.ascii_uppercase, k=20))
