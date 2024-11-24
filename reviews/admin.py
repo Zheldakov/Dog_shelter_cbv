4ceb2e8de2363483536cfedc8560f82c1a5ee796
Zheldakov Andrey <zheldakov.aa@gmail.com>
2024-11-24 18:49:23 +0300

    Добавление комментариев

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,6 +3,7 @@ from reviews.models import  Review
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
+    """Отображение в админ панели"""
     list_display = ('title','dog','author','created', 'sign_of_review')
     ordering = ('created',)
     list_filter = ('dog','author',)
